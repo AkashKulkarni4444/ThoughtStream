22ac5a0834821c0a5fbf54c7ce559dafa3e6bc92
Akash Kulkarni <akashkulkarni4444@gmail.com>
2024-06-14 18:43:25 +0530

    1) Added peotected routes using middlewares. 2) Added Dashboard to add workspace 3)Added emoji picker 4)Use Toast created. 5)Added CreateWorkspaceFormSchema type 6) Added appwide state provider 7) Added supabase queries and types.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import db from "@/lib/supabase/db";
 import { ThemeProvider } from "@/lib/providers/next-theme-provider";
 import { DM_Sans } from "next/font/google";
 import { twMerge } from "tailwind-merge";
+import AppStateProvider from "@/lib/providers/state-provider";
 
 const inter = DM_Sans({ subsets: ["latin"] });
 
@@ -21,9 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={twMerge('bg-background',inter.className)}>
+      <AppStateProvider>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
         </ThemeProvider>
+      </AppStateProvider>
       </body>
     </html>
   );
